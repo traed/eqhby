d4ecc9d7bb67f777c58921239392f6d6b79e1d01
Mattias Forsman <forsman1991@gmail.com>
2019-05-21 09:09:36 +0200

    - Increase default font size - Add narrow page template

---
+++ page_narrow.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template Name: Narrow layout
+ *
+ * This template is meant for pages that mainly contain text. It has a narrow layout for better readability.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package eqhby
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area narrow">
+		<main id="main" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
