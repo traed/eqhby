9feeb6922377eff209638b4b09e70bc1650ad97b
Mattias Forsman <forsman1991@gmail.com>
2019-06-23 13:12:39 +0200

    Improved sub menu position

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -4,4 +4,22 @@ jQuery(document).ready(function($) {
 		edge: 'right',
 		closeOnClick: true
 	});
+
+	var $submenus = $('.main-menu > li > .sub-menu');	
+	if($submenus.length > 0) {
+		for(var i = 0; i < $submenus.length; i++) {
+			var $submenu = $submenus.eq(i);
+			var submenuOffsetLeft = $submenu.offset().left;
+			var submenuOffsetRight = submenuOffsetLeft + $submenu.width();
+			var menuItemOffsetLeft = $submenu.parent().offset().left;
+			var menuItemOffsetRight = menuItemOffsetLeft + $submenu.parent().width();
+	
+			if(menuItemOffsetLeft < submenuOffsetLeft || menuItemOffsetRight > submenuOffsetRight) {
+				var newLeft = menuItemOffsetLeft - $('.main-menu').parent().offset().left + ($submenu.parent().width() / 2);
+				$submenu.css({
+					left: parseInt(newLeft) + 'px'
+				});
+			}
+		}
+	}
 });
